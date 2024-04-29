8b2c4c16bf941b18109865d10738f0189e4f3c8e
HarrietEkisa <harrietekisa@gmail.com>
2024-04-29 09:53:44 -0700

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,9 +11,8 @@ from urllib.parse import urlencode
 
 if __name__ == "__main__":
     data = urlencode({
-                        'email': argv[2]
-                    }).encode('ascii')
-    req = Request(argv[1], data)
-
-    with urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+'email': argv[2]
+}).encode('ascii')
+req = Request(argv[1], data)
+with urlopen(req) as res:
+print(res.read().decode('utf-8'))
